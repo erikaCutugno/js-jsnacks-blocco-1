a0ace491c5120edc54614695f3ab1fcd991e7aff
erikaCutugno <erikucciapop@gmail.com>
2024-12-03 12:46:09 +0100

    fix: exe2

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -3,8 +3,9 @@ const party = ["Marco", "Maria", "Jessica","Leonardo"]
 const userName = prompt("Inserisci il tuo nome") 
 
 for (let i = 0; i < party.length; i++){
-    if(party[i]=== userName){
-    console.log(party[i])
-    alert("Puoi partecipare")
+    if(party[i] === userName){
+        alert("Puoi partecipare")
+    } else {
+        alert("Non puoi partecipare")
     }
 }
