582382802638c04141cda5fc98203d956d556bc3
erikaCutugno <erikucciapop@gmail.com>
2024-12-03 11:32:47 +0100

    feat: exe 1

---
+++ jsnack1/script.js
@@ -0,0 +1,8 @@
+const userfirst = Number(prompt("Inserisci un numero"))
+const usersecond = Number(prompt("Inserisci un numero"))
+
+if(userfirst > usersecond) {
+    alert(`Il maggiore è ${userfirst}`)
+} else {
+    alert(`Il maggiore è ${usersecond}`)
+}
